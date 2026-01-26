72d7e60ed4415da812fbc095843949c219d64b33
chanan-duy <chanan.duy@proton.me>
2026-01-26 17:36:58 +0500

    Migration

--- LogKt01/Migrations/20260126123536_01_TaskDateTime.Designer.cs
+++ LogKt01/Migrations/20260126123536_01_TaskDateTime.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using LogKt01.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LogKt01.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260126123536_01_TaskDateTime")]
+    partial class _01_TaskDateTime
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.2");
