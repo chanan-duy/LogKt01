a698c9469368a6599d137561f401ccd68c02aa16
chanan-duy <chanan.duy@proton.me>
2026-01-26 17:30:16 +0500

    Add migration

---
+++ LogKt01/Migrations/20260126123004_00_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LogKt01.Migrations
+{
+    /// <inheritdoc />
+    public partial class _00_InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tasks",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    title = table.Column<string>(type: "TEXT", maxLength: 80, nullable: false),
+                    category = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    is_done = table.Column<bool>(type: "INTEGER", nullable: false),
+                    marked_as_removed = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tasks", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tasks");
+        }
+    }
+}
